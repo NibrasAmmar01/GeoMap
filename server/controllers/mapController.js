6ed9e9170830e160c0502700c98a4efbf3370f09
Nibras Ammar <nibras.ammar@etudiant-enit.utm.tn>
2025-10-29 15:11:35 +0100

    Add server controllers, routes, and update index.js

---
+++ server/controllers/mapController.js
@@ -0,0 +1,31 @@
+const db = require('../db');
+
+module.exports.addName = (req, res) => {
+  const name = req.body.name;
+  const checkName = "SELECT * FROM store WHERE name = ?";
+
+  db.query(checkName, [name], (err, response) => {
+    if (err) {
+      console.error("Error checking name:", err);
+      return res.status(500).json({ msg: "Database error while checking name." });
+    }
+
+    if (response.length > 0) {
+      return res.status(422).json({ msg: "This place is already added to the list!" });
+    }
+
+    const insertQuery = "INSERT INTO store(name) VALUES(?)";
+    db.query(insertQuery, [name], (err, result) => {
+      if (err) {
+        console.error("Error inserting name:", err);
+        return res.status(500).json({ msg: "Database error while adding name." });
+      }
+
+      if (result.affectedRows > 0) {
+        return res.status(200).json({ msg: "Added place successfully" });
+      } else {
+        return res.status(400).json({ msg: "Something went wrong" });
+      }
+    });
+  });
+};
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const env = require('dotenv'); // to store constant variables in env file
 const cors = require('cors'); // to prevent cors policy error
 
 const db = require('./db');
+const router = require('./routes/mapRoute');
 
 
 
@@ -15,6 +16,8 @@ const port = process.env.port;
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api',router)
 
 //listen to port 2000
 app.listen(port,()=>{
